42b8e7445f0811a8e6c569ee869650bf9b5825b6
Andrew Nesbitt <andrewnez@gmail.com>
2015-01-03 21:24:04 +0000

    Added rspec test config block

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,17 +5,11 @@ require 'split/helper'
 require 'split/analytics'
 require 'ostruct'
 
-def session
-  @session ||= {}
-end
-
-def params
-  @params ||= {}
-end
-
-def request(ua = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; de-de) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27')
-  r = OpenStruct.new
-  r.user_agent = ua
-  r.ip = '192.168.1.1'
-  @request ||= r
+RSpec.configure do |config|
+  config.order = 'random'
+  config.before(:each) do
+    Split.configuration = Split::Configuration.new
+    Split.redis.flushall
+    @ab_user = {}
+  end
 end
