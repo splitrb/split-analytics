22b1c37f0e9bbcb409e36805a5219660c8217d2c
Andrew Nesbitt <andrewnez@gmail.com>
2015-01-03 21:35:09 +0000

    Use ab_user instead of session

--- spec/analytics_spec.rb
+++ spec/analytics_spec.rb
@@ -21,8 +21,6 @@ describe Split::Analytics do
     first_alt = ab_test('link_colour', 'red', 'blue')
     second_alt = ab_test('link_text', 'Join', 'Signup')
 
-    session[:split].should eql({'link_colour' => first_alt,'link_text' => second_alt})
-
     tracking_code = tracking_code(:account => 'UA-12345-6')
     tracking_code.should eql("        <script type=\"text/javascript\">\n          var _gaq = _gaq || [];\n          _gaq.push(['_setAccount', 'UA-12345-6']);\n          _gaq.push(['_setCustomVar', 1, 'link_colour', '#{first_alt}', 1]);\n_gaq.push(['_setCustomVar', 2, 'link_text', '#{second_alt}', 1]);\n\n          (function() {\n            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\n            ga.src = ('https:' == document.location.protocol ? 'https://ssl.google-analytics.com/ga.js' : 'http://www.google-analytics.com/ga.js');\n            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\n          })();\n        </script>\n")
   end
