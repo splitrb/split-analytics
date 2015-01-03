a1a86502a039634b9b41b65dd17d91766c792f6d
Andrew Nesbitt <andrewnez@gmail.com>
2015-01-03 22:15:00 +0000

    Fixed custom variables spec

--- lib/split/analytics.rb
+++ lib/split/analytics.rb
@@ -31,9 +31,9 @@ module Split
     end
 
     def custom_variables
-      return nil if ab_user[:split].nil?
+      return nil if ab_user.nil?
       arr = []
-      ab_user[:split].each_with_index do |h,i|
+      ab_user.each_with_index do |h,i|
         arr << "_gaq.push(['_setCustomVar', #{i+1}, '#{h[0]}', '#{h[1]}', 1]);"
       end
       arr.reverse[0..4].reverse.join("\n")
